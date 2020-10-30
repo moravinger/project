73489b4c9a827e7023eb9ed661f641bc68e757c4
Dima <moravinger@gmail.com>
2020-10-31 00:16:28 +0500

    первое домашнее задание

--- js/script.js
+++ js/script.js
@@ -44,5 +44,28 @@
 // console.log(++incr);
 // console.log(--decr);
 
-let numberOfFilms = prompt ('Сколько фильмов вы уже посмотрели?');
-console.log(numberOfFilms);
+//Первое домашнее задание (часть 1)
+
+const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?', ''); //1 домашнее задание
+// console.log(numberOfFilms);
+
+//Первое домашнее задание (часть 2)
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    prvat: false
+};
+
+//Первое домашнее задание (часть 3)
+const a = prompt ('Один из последних просмотренных фильмов?', ''),
+    b = prompt ('На сколько оцените его?', ''),
+    c = prompt ('Один из последних просмотренных фильмов?', ''),
+    d = prompt ('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
