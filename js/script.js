5c4641cfba252e074bd1ca87d1b36c157ba14e22
Dima <moravinger@gmail.com>
2020-10-30 23:28:16 +0500

    первое домашние задание

--- js/script.js
+++ js/script.js
@@ -1,45 +1,48 @@
 "use strict";
-// let number = 5;
-// const leftBorderWidth = 1;
+// // let number = 5;
+// // const leftBorderWidth = 1;
 
-// number = 10;
-// console.log(number);
+// // number = 10;
+// // console.log(number);
 
-// const obj = {
-//     name: 'Дима',
-//     age: '29',
-//     isMarried: false
-// };
+// // const obj = {
+// //     name: 'Дима',
+// //     age: '29',
+// //     isMarried: false
+// // };
 
-// console.log(obj.age);
-// // console.log(obj['name']); 
+// // console.log(obj.age);
+// // // console.log(obj['name']); 
 
-// // alert('Hello');
+// // // alert('Hello');
 
-// // const result = confirm('Are you here?');
-// // console.log(result);
+// // // const result = confirm('Are you here?');
+// // // console.log(result);
 
-// // const answer = +prompt('Вам есть 18?',''); //+ превращает строку в число 
-// // console.log(answer + 5);
+// // // const answer = +prompt('Вам есть 18?',''); //+ превращает строку в число 
+// // // console.log(answer + 5);
 
-// const answers = [];
+// // const answers = [];
 
-// // answers[0] = prompt('Как ваше имя?', '');
-// // answers[1] = prompt('Как ваше фамилия?', '');
-// // answers[2] = prompt('Сколько вам лет?', '');
+// // // answers[0] = prompt('Как ваше имя?', '');
+// // // answers[1] = prompt('Как ваше фамилия?', '');
+// // // answers[2] = prompt('Сколько вам лет?', '');
 
-// console.log(typeof(answers));
+// // console.log(typeof(answers));
 
-// const category = 'toys';
-// console.log(`https://someurl.com/${category}/5`);
+// // const category = 'toys';
+// // console.log(`https://someurl.com/${category}/5`);
 
-// const user = 'Дима';
-// alert(`Привет, ${user}`);
+// // const user = 'Дима';
+// // alert(`Привет, ${user}`);
 
-let incr = 10,
-    decr = 10;
-// ++incr; //инкримент добавляет +1
-// --decr; //дикримент отимает 1
+// let incr = 10,
+//     decr = 10;
+// // ++incr; //инкримент добавляет +1
+// // --decr; //дикримент отимает 1
 
-console.log(++incr);
-console.log(--decr);
+// console.log(++incr);
+// console.log(--decr);
+
+let numberOfFilms = prompt ('Сколько фильмов вы уже посмотрели?');
+console.log(numberOfFilms);
