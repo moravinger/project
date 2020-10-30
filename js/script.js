fd71092efdaba7372d323aa633c388186ee4cbd1
Dima <moravinger@gmail.com>
2020-10-30 22:28:33 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,45 @@
+"use strict";
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     name: 'Дима',
+//     age: '29',
+//     isMarried: false
+// };
+
+// console.log(obj.age);
+// // console.log(obj['name']); 
+
+// // alert('Hello');
+
+// // const result = confirm('Are you here?');
+// // console.log(result);
+
+// // const answer = +prompt('Вам есть 18?',''); //+ превращает строку в число 
+// // console.log(answer + 5);
+
+// const answers = [];
+
+// // answers[0] = prompt('Как ваше имя?', '');
+// // answers[1] = prompt('Как ваше фамилия?', '');
+// // answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Дима';
+// alert(`Привет, ${user}`);
+
+let incr = 10,
+    decr = 10;
+// ++incr; //инкримент добавляет +1
+// --decr; //дикримент отимает 1
+
+console.log(++incr);
+console.log(--decr);
